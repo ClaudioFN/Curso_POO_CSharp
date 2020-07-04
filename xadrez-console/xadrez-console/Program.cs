adb5bcdb96a2e09dbf4f8f9c51dc557966fe2508
ClaudioFN <clawdio30@gmail.com>
2020-07-04 16:17:04 -0300

    Novas Classes para tabuleiro - A152
    Commit das classes Cor, Peca e Tabuleiro - A152

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -4,9 +4,7 @@ using tabuleiro;
 namespace xadrez_console{
     class Program{
         static void Main(string[] args){
-            Posicao P;
-
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             
         }
     }
