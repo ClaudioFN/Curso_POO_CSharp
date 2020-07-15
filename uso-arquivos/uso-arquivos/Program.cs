fce3676bb29063f9f200a35fe2dfb1f0a3040d0d
ClaudioFN <clawdio30@gmail.com>
2020-07-14 22:47:29 -0300

    Tratativas de Arquivos - A186_1
    Commit dos itens inciais relacionados a arquivos - A186_1

---
+++ uso-arquivos/uso-arquivos/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace uso_arquivos {
+    class Program {
+        // Aula 1 - 186
+        static void Main(string[] args) {
+            Console.WriteLine("Oloco bixo!");
+        }
+    }
+}
