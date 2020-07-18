ea9eb55e8df30496ffb0d50dd023c9448c49f935
ClaudioFN <clawdio30@gmail.com>
2020-07-18 17:10:35 -0300

    Path - A191
    Uso de Path para obter detalhes dos arquivos e informações sobre eles e alguns detalhes de diretórios. - A191

--- uso-arquivos/uso-arquivos/Program.cs
+++ uso-arquivos/uso-arquivos/Program.cs
@@ -134,13 +134,21 @@ namespace uso_arquivos {
         */
 
         /* Aula 6 - 191
-        *  Path
-        */
+        *  Path - Diversos usos para obter detalhes de pastas e arquivos
+        
         static void Main(string[] args) {
             string path = @"C:\Users\claudio\Documents\Mobizen\file1.txt";
 
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
             Console.WriteLine("GetDirectoryName : " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
 
         }
+        */
     }
 }
