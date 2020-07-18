363ac5256876d1f81afc3fcfce12346775f9e67b
ClaudioFN <clawdio30@gmail.com>
2020-07-18 09:23:49 -0300

    Uso de File e FileInfo - A186_2
    Demonstração de uso de cópia de arquivos pelo FileInfo e leitura de arquivos através do File.

--- uso-arquivos/uso-arquivos/Program.cs
+++ uso-arquivos/uso-arquivos/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace uso_arquivos {
     class Program {
-        // Aula 1 - 186
+        /* Aula 1 - 186
+        *  FileInfo - Função de Cópia(CopyTo) que não permite sobrescrita
+        *  File - Ler conteúdo de arquivo(ReadAllLines)
+        
         static void Main(string[] args) {
-            Console.WriteLine("Oloco bixo!");
+            string sourcePath = @"C:\temp\file1.txt"; // Origem
+            string targetPath = @"C:\temp\file2.txt"; // Destino onde ainda não existe o arquivo
+            try {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath); // Faz a cópia do arquivo
+                string[] lines = File.ReadAllLines(sourcePath); // Ler linhas do arquivo selecionado
+                int i = 0;
+                foreach(string line in lines) {
+                    Console.WriteLine("Linha " + ++i + " - " + line);
+                }
+            }catch(IOException e) {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
         }
+        */
     }
 }
