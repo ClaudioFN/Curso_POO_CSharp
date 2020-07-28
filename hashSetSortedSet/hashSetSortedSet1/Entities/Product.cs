72a9bbd1d8aa4c3434f6891a79adf6028115fbf7
ClaudioFN <clawdio30@gmail.com>
2020-07-27 21:30:21 -0300

    HashSet e SortedSet - A208 até A210
    Implementações de HashSet e SortedSet com uso de GetHashCode e Equals. - A208 até A210

---
+++ hashSetSortedSet/hashSetSortedSet1/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities {
+    class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product (string name, double price) {
+            Name = name;
+            Price = price;
+        }
+        // Implementar o GetHashCode e o Equals faz com que a compração deixe de ser por referência de memória
+        // e seja feita pela regra estabelecida
+        public override int GetHashCode() {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Product)) {
+                return false;
+            }
+            Product other = obj as Product;
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
---
+++ hashSetSortedSet/hashSetSortedSet1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+
+
+namespace hashSetSortedSet1 {
+    class Program {
+        /*static void Main(string[] args) {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            foreach (string p in set) {
+                Console.WriteLine("Values: " + p);
+            }
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            PrintCollection(a);
+
+            // Union - Juntar os dois conjuntos sem repitir números
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            // Intersection - Mantém os elementos que são iguais em ambos 
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            // Difference - Mostra os elementos que existem no item 1 mas não no item 2
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+        }
+        static void PrintCollection<T>(IEnumerable<T> collection) {
+            foreach (T obj in collection) {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }*/
+        static void Main(string[] args) {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.0));
+            a.Add(new Product("Notebook", 1200.0));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.0);
+            Console.WriteLine(a.Contains(prod));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p)); // Comparação por conteúdo e não por referência
+        }
+    }
+}
