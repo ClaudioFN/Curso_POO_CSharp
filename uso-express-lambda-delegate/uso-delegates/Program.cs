1b76063177b0aa8de8141d552cd99baa1f3d6b07
ClaudioFN <clawdio30@gmail.com>
2020-08-15 16:44:52 -0300

    Principio de Delegate - A221
    Criacao de Projeto Delegate com uso pratico. - A221

---
+++ uso-express-lambda-delegate/uso-delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uso_delegates {
+
+    // Delegate: type safety de declaracoes
+    delegate double BinaryNumericOperation(double n1, double n2); // O delegate deve ser declarado seguindo a mesma logica de parametrizacao que o metodo referenciado
+    class Program {
+        static void Main(string[] args) {
+            double a = 10;
+            double b = 12;
+
+            double result = CalculationService.Max(a, b);
+            double result2 = CalculationService.Sum(a, b);
+            double result3 = CalculationService.Square(a);
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            Console.WriteLine("Result: " +  result + "\nResult2: " + result2 + "\nResult3: " + result3);
+            Console.WriteLine("\nOperation: " + op(a, b));
+
+        }
+
+        
+    }
+}
