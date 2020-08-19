886fc732f53163101700663316c20c302c0b469c
ClaudioFN <clawdio30@gmail.com>
2020-08-18 22:49:05 -0300

    Uso de Delegate Func - A225
    Criacao de classe Product para demosntracao do Delegate Func com Select para identificar itens da lista. -A225

--- uso-express-lambda-delegate/uso-delegates/Program.cs
+++ uso-express-lambda-delegate/uso-delegates/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace uso_delegates {
 
     // Delegate: type safety de declaracoes
     delegate double BinaryNumericOperation(double n1, double n2); // O delegate deve ser declarado seguindo a mesma logica de parametrizacao que o metodo referenciado
+
     class Program {
+        /*
+         * Delegate comentado na aula 225
         static void Main(string[] args) {
             double a = 10;
             double b = 12;
@@ -20,6 +26,34 @@ namespace uso_delegates {
 
         }
 
-        
+        */
+
+        // Delegate Func: uso direto com listas e que tem retorno em IEnumerable
+        static void Main(string[] args){
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.0));
+            list.Add(new Product("Mouse", 50.0));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("Hd Case", 80.90));
+
+            Product p;
+
+            Func<Product, string> func = NameUpper;
+            Func<Product, string> func2 = p => p.Name.ToUpper();
+            //List<string> result = list.Select(NameUpper).ToList(); // Chamada de metodo direta
+            //List<string> result = list.Select(func).ToList(); // Chamada via Delegate Func
+
+            List<string> result = list.Select(func2).ToList();
+            List<string> result2 = list.Select(p => p.Name.ToUpper()).ToList(); // Colocar a expressao lambda direto funciona
+
+            foreach (string s in result) {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p) {
+            return p.Name.ToUpper();
+        }
     }
 }
