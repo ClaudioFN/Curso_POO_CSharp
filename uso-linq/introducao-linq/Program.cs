52c34e8deba2e941828ba7b12be8bc881a793131
ClaudioFN <clawdio30@gmail.com>
2020-08-22 23:21:23 -0300

    Introducao ao LINQ - A226 e A227
    Introducao a LINQ, exemplificacao e alguns comandos uteis. -A226 e A227

---
+++ uso-linq/introducao-linq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace introducao_linq {
+    class Program {
+        static void Main(string[] args) {
+            // Especificar a fonte de dados
+            int [] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Definir a expressão da query 
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Executar a query
+            foreach (int x in result) {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
